facc6081f1f8a415968d03e1c048ca456827d556
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2022-12-05 14:51:53 -0300

    Added basic journalView file code and transfered HTML code to be rendered by journalView

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-import FilesAPI from "./filesAPI";
+import FilesView from "./journalView";
 
+const mainHtmlElement = document.querySelector(".main");
 const richTextEditorButtonsElement = document.querySelectorAll(".btn");
+
+const view = new FilesView(mainHtmlElement, {
+  onFileSelect() {
+    console.log("File has been selected!");
+  },
+});
 
 richTextEditorButtonsElement.forEach((button) => {
   button.addEventListener("click", () => {
